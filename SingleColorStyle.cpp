b283f5724ec1b07a01b6d2ad35afc85bbc3e52d3
Larry Moore <lfmoore3@hotmail.com>
2023-03-04 15:49:22 -0500

    Make single color style solid only

--- SingleColorStyle.cpp
+++ SingleColorStyle.cpp
@@ -4,78 +4,17 @@
 
 SingleColorStyle::SingleColorStyle(String name, uint32_t color, PixelBuffer* pixelBuffer) : LightStyle(name, pixelBuffer) {
   m_color = color;
-  m_iterationCount = 0;
-  m_nextUpdate = 0;
 }
 
 void SingleColorStyle::update() {
-  if (millis() < m_nextUpdate) {
-    return;
-  }
-
-  int mod = getModulus();
-  uint32_t newColor = m_color;
-  if (mod > 0 && m_iterationCount % mod == 0) {
-    newColor = 0;
-  }
-
-  shiftColorUsingPattern(newColor);
-  m_iterationCount++;
-  m_nextUpdate = millis() + getIterationDelay();
+  // Single color style does not blink or anything.
+  // Update does nothing.
 }
 
 void SingleColorStyle::reset()
 {
-  int mod = getModulus();
-  int numBlocks = getNumberOfBlocksForPattern();
-  if (numBlocks > 100) {
-    // The only patterns with this many blocks are the line patterns.
-    // Instead of shifting tons of times, just set the pixels directly.
-    for (int i = 0; i < numBlocks; i++) {
-      if (mod > 0 && i % mod == 0) {
-        m_pixelBuffer->setPixel(i, 0);
-      } else {
-        m_pixelBuffer->setPixel(i, m_color);
-      }
-    }
-  }
-
-  for (int i = 0; i < numBlocks; i++) {
-    if (mod > 0 && i % mod == 0) {
-      shiftColorUsingPattern(0);
-    } else {
-      shiftColorUsingPattern(m_color);
-    }
+  // Set all pixels to a single color.
+  for (int i = 0; i < m_pixelBuffer->getPixelCount(); i++) {
+    m_pixelBuffer->setPixel(i, m_color);
   }
 }
-
-int SingleColorStyle::getIterationDelay() {
-  // Convert "speed" to a delay.
-  // Speed ranges from 1 to 100.
-  int minDelay = 20;
-  int maxDelay = 1000;
-  double m = (maxDelay - minDelay)/-99.0;
-  double b = maxDelay - m;
-  int delay = m_speed*m + b;
-  return delay;
-}
-
-int SingleColorStyle::getModulus() {
-  // Convert "step" to a modulus -- every "modulus" pixel will be off.
-  // Step ranges from 1 to 100.
-  int minStep = 1; 
-  int maxStep = 95; // any values higher than this will result in all pixels being on.
-  int minMod = 2;
-  int maxMod = 10;
-
-  if (m_step > maxStep) {
-    return -1;
-  }
-
-  double m = (double)(maxMod - minMod)/(maxStep - minStep);
-  double b = minMod - m;
-  int mod = m_step*m + b;
-  return mod;
-}
-
-
