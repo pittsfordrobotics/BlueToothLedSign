aa636b08925b203ea06edfa3c5e0310e84e4c1a1
Larry Moore <lfmoore3@hotmail.com>
2023-03-04 15:49:04 -0500

    Reorder styles; Simplify patterns

--- LightStyle.cpp
+++ LightStyle.cpp
@@ -9,14 +9,12 @@ LightStyle::LightStyle(String name, PixelBuffer* pixelBuffer) {
   m_name = name;
   if (knownPatterns.size() == 0) {
     knownPatterns.push_back("Solid");
-    knownPatterns.push_back("ColumnRight");
-    knownPatterns.push_back("ColumnLeft");
-    knownPatterns.push_back("RowUp");
-    knownPatterns.push_back("RowDown");
-    knownPatterns.push_back("DigitRight");
-    knownPatterns.push_back("DigitLeft");
-    knownPatterns.push_back("LineRight");
-    knownPatterns.push_back("LineLeft");
+    knownPatterns.push_back("Right");
+    knownPatterns.push_back("Left");
+    knownPatterns.push_back("Up");
+    knownPatterns.push_back("Down");
+    knownPatterns.push_back("Digit");
+    knownPatterns.push_back("Random");
   }
 }
 
@@ -60,29 +58,23 @@ void LightStyle::shiftColorUsingPattern(uint32_t newColor) {
   // Stick with integer values here instead of doing a bunch
   // of "ifs" to compare strings.  
   switch (m_pattern) {
-    case 1:
+    case 1: // Right
       m_pixelBuffer->shiftColumnsRight(newColor);
       return;
-    case 2:
+    case 2: // Left
       m_pixelBuffer->shiftColumnsLeft(newColor);
       return;
-    case 3:
+    case 3: // Up
       m_pixelBuffer->shiftRowsUp(newColor);
       return;
-    case 4:
+    case 4: // Down
       m_pixelBuffer->shiftRowsDown(newColor);
       return;
-    case 5:
+    case 5: // Digit
       m_pixelBuffer->shiftDigitsRight(newColor);
       return;
-    case 6:
-      m_pixelBuffer->shiftDigitsLeft(newColor);
-      return;
-    case 7:
+    case 6: // "Random" - really linear, but appears sort of random on the real sign
       m_pixelBuffer->shiftLineRight(newColor);
-      return;
-    case 8:
-      m_pixelBuffer->shiftLineLeft(newColor);
       return;
     default:
       // Default to Solid (ie, all lights the same color)
